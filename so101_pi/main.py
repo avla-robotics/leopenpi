f9b74a8e82ad98b14eadd989c0cafada5d20d9a9
Skyler <skylerwiernik@gmail.com>
2025-09-25 21:09:44 -0400

    feat: openpi flow

--- so101_pi/main.py
+++ so101_pi/main.py
@@ -1,8 +1,19 @@
 from draccus import parse
+from lerobot_wrapper import RobotWrapper
+from openpi_client.runtime.agents.policy_agent import PolicyAgent
+from openpi_client.runtime.runtime import Runtime
+from openpi_client.websocket_client_policy import WebsocketClientPolicy
 from so101_pi.utils.environment_configuration import EnvironmentConfiguration
+from so101_pi.robot_environment import RobotEnvironment
 
-def main(environment_configuration: EnvironmentConfiguration):
-    print(environment_configuration)
+def main(config: EnvironmentConfiguration):
+    robot = RobotWrapper(config.robot_port)
+    environment = RobotEnvironment(config.prompt, robot, config.cameras)
+    policy = WebsocketClientPolicy(host=config.server_ip, port=config.server_port)
+    agent = PolicyAgent(policy)
+    runtime = Runtime(environment, agent, [])
+    runtime.run()
+
 
 
 if __name__ == "__main__":
