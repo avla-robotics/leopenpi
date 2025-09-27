a36661d215c9d7a7bc1b103683eeb7739be0980b
Skyler <skylerwiernik@gmail.com>
2025-09-27 14:08:50 -0400

    fix: imports

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from draccus import parse
 from openpi_client.runtime.agents.policy_agent import PolicyAgent
 from openpi_client.runtime.runtime import Runtime
 from openpi_client.websocket_client_policy import WebsocketClientPolicy
-from .utils import EnvironmentConfiguration, LoggingSubscriber, RobotWrapper
-from .robot_environment import RobotEnvironment
+from utils import EnvironmentConfiguration, LoggingSubscriber, RobotWrapper
+from robot_environment import RobotEnvironment
 
 def main(config: EnvironmentConfiguration):
     robot = RobotWrapper(config.robot, config.logger)
